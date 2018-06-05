3e4fb68a8cfc7671b3e31a66c25814b391fd8b9e
ПК <@192.168.0.84>
2018-06-06 00:38:00 +0500

    lab2_2

---
+++ javaapplication/Lab2_2.java
@@ -0,0 +1,42 @@
+
+package lab2_2;
+
+import java.util.Scanner;
+
+class MyUtility {
+
+	private MyUtility() 
+	{
+		throw new AssertionError();
+	}
+
+	static Scanner sc = new Scanner(System.in);
+
+	static public int getInt()
+	{
+		while(!sc.hasNextInt()) {
+			System.out.println("Invalid data!");
+			sc.next();
+		}
+		return sc.nextInt();
+	}
+	
+	
+}
+
+
+public class Lab2_2 {
+
+    public static void main(String[] args)
+	{
+		System.out.print("Введите m и n для построения прямоугольника: ");
+
+		int row = MyUtility.getInt(), col = MyUtility.getInt();
+
+		for(int i = 0; i < row; ++i) {
+			for(int j = 0; j < col; ++j)
+				System.out.print("8");
+			System.out.println();
+		}
+	}
+}
