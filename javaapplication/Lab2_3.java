365e565e611d9d64d32efabc848ddb0adbad9207
ПК <@192.168.0.84>
2018-06-06 00:41:57 +0500

    lab2_3

---
+++ javaapplication/Lab2_3.java
@@ -0,0 +1,19 @@
+
+package lab2_3;
+
+public class Lab2_3 {
+
+    public static void main(String[] args)
+	{
+		System.out.println("Прямоугольный треугольник 10x10:");
+
+		for(int i = 0; i < 10; ++i) {
+			for(int j = 0; j < 10; ++j) {
+				if(j > i) break;
+				else System.out.print("8");
+			}
+			System.out.println();
+		}
+	}
+    
+}
