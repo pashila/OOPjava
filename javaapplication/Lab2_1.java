ef7d3187cd00625d542feb99cab19ada153db0d1
ПК <@192.168.0.84>
2018-06-06 00:33:31 +0500

    lab2_1

---
+++ javaapplication/Lab2_1.java
@@ -0,0 +1,13 @@
+
+package lab2_1;
+
+public class Lab2_1 {
+
+   public static void main(String[] args){
+        int i;
+        for(i=2;i<=100;i+=2){
+            System.out.println(i);
+            }
+        }
+    
+}
