c1890f43cb7159d6f1f5a09ddec06030028b8a87
ПК <@192.168.0.84>
2018-06-06 01:18:42 +0500

    @pashila_

--- javaapplication/Lab2_4_3.java
+++ javaapplication/Lab2_4_3.java
@@ -1,9 +1,5 @@
-/*
- *
- * Finding a point on the Axis
- *
- */
-package lab_2_4_3;
+
+package lab2_4_3;
 import java.util.Scanner;
 
 class Coordinate {
@@ -28,11 +24,11 @@ class Coordinate {
 
 /**
  *
- * @author UI3es
+ * @author pashila
  */
-public class Lab_2_4_3 {
+public class Lab2_4_3 {
 
-    private Lab_2_4_3() {
+    private Lab2_4_3() {
         throw new AssertionError();
     }
 
@@ -52,19 +48,19 @@ public class Lab_2_4_3 {
     public static void main(String[] args) {
         
         System.out.print (
-            "Enter the coordinates of the point not lying on the coordinate axes: "
+            "Введите координаты точки, не лежащей на координатных осях: "
         );
 
         Coordinate print = new Coordinate(
-            Lab_2_4_3.getInt(),
-            Lab_2_4_3.getInt()
+            Lab2_4_3.getInt(),
+            Lab2_4_3.getInt()
         );
         
         int quarter = print.getQuarter();
         
         String outputString;
         
-        if(quarter == 0) outputString = "The point lies on the axis";
+        if(quarter == 0) outputString = "Точка лежит на оси";
         else outputString = "Точка лежит в " + Integer.toString(quarter) + " четверти";
 
         System.out.println(outputString);
